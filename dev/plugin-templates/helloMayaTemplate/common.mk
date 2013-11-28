ae78a9728a928174afb913c57a19647db3a77319
U-nebuchadnezzar\kbrown <kbrown@nebuchadnezzar.(none)>
2013-11-28 23:47:18 +0000

    windoze stuff

--- dev/plugin-templates/helloMayaTemplate/common.mk
+++ dev/plugin-templates/helloMayaTemplate/common.mk
@@ -15,7 +15,7 @@ AM_LDFLAGS += -L$(MAYA_LOCATION)/Maya.app/Contents/MacOS -Wl,-executable_path,$(
 endif
 
 if HOST_SYS_WINDOWS
-AM_CXXFLAGS += -spam
+AM_CXXFLAGS += /c /I. /I"$(MAYA_LOCATION)\include" /nologo /W3 /WX- /O2 /Ob1 /D NDEBUG /D WIN32 /D _WINDOWS /D _USRDLL /D NT_PLUGIN /D _HAS_ITERATOR_DEBUGGING=0 /D _SECURE_SCL=0 /D _SECURE_SCL_THROWS=0 /D _SECURE_SCL_DEPRECATE=0 /D _CRT_SECURE_NO_DEPRECATE /D TBB_USE_DEBUG=0 /D __TBB_LIB_NAME=tbb.lib /D REQUIRE_IOSTREAM /D AW_NEW_IOSTREAMS /D Bits64_ /D _WINDLL /GF /Gm- /EHsc /MD /GS /Gy /fp:precise /Zc:wchar_t /Zc:forScope /GR /Gd /TP
 
-AM_LDFLAGS += -eggs
+AM_LDFLAGS += /OUT:"MayaPluginWizard1.mll" /INCREMENTAL:NO /NOLOGO /LIBPATH:"C:\Program Files\Autodesk\Maya2013\lib" Foundation.lib OpenMaya.lib OpenMayaUI.lib OpenMayaAnim.lib OpenMayaFX.lib OpenMayaRender.lib Image.lib libMocap.lib OpenGL32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /TLBID:1 /DYNAMICBASE:NO /NXCOMPAT /IMPLIB:"MayaPluginWizard1.lib" /MACHINE:X64 /DLL MayaPluginWizard1Node.obj pluginMain.obj /export:initializePlugin /export:uninitializePlugin
 endif
